b07084e7f503d566e54ef9faa9c01126213f22e5
Work <work@Hrags-MacBook-Pro.local>
2025-05-13 12:00:57 -0400

    page rendering functionality

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -8,3 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, page_name):
+    if util.get_entry(page_name):
+        return render(request, "encyclopedia/page.html", {
+            "page_name": page_name,
+            "render": markdown2.markdown(util.get_entry(page_name))
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "error": f"Error 404 page name: {page_name} was not found! "
+        })
